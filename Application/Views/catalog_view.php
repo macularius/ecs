355151bb6264bf129420a74ae81012cbcc07c848
Macularius <igor.kovatsenko@yandex.ru>
2018-11-02 20:46:47 +0400

    bootstrap adapt to layout 2 completed

--- Application/Views/catalog_view.php
+++ Application/Views/catalog_view.php
@@ -1,7 +1,7 @@
-<div class="container-fluid">
+<div class="container-fluid goods">
     <div class="row">
         <?php
-            for ($i=1; $i<50; $i++){
+            for ($i=1; $i<49; $i++){
                 echo "<div class=\"good col-12 col-md-4 col-xl-3\">good $i</div>";
             }
         ?>
